069d2f1630b41d00ebd8717c331f5c6238e79137
Vijayashree Karanth <vkaranth@in.ibm.com>
2017-05-10 06:48:56 +0000

    Making changes again

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  license_key: 'd633dcda49553df0a982fc23c6b41a2f9792a672',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
